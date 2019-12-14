917eccd63a4539a19963c3feae33e3f0a370c558
Juan <juan.rosales@bicasoft.com>
2019-12-14 09:07:39 -0300

    Generator

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 var pdf = require('html-pdf');
-var requestify = require('requestify');
 var bodyParser = require('body-parser');
+var uniqid = require('uniqid');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,41 +16,40 @@ const mocks = require ('./mocks/mocks');
 app.get('/:report', function(req, res) {
     const reportId = req.params.report;
     const params =  mocks[reportId];
-    console.log(params);
-    res.render(`pages/${reportId}` , {params});
+    res.render(`pages/${reportId}`, {params});
 });
+
+const buildPdf = (reportId, html, res) => {
+  // Get the raw HTML response body
+  var config = {format: 'A4'}; // or format: 'letter' - see https://github.com/marcbachmann/node-html-pdf#options
+
+ // Create the PDF
+  const fileName = `${reportId}-${uniqid()}.pdf`;
+  pdf.create(html, config).toFile(fileName, function (err, result) {
+     if (err) return console.log(err);
+     console.log(result);
+     // res.sendFile('generated.pdf',{ root: __dirname });
+     res.send({
+      success: true,
+      file: fileName
+    });
+  });
+}
 
 app.post('/:report', function(req, res) {
   const reportId = req.params.report;
   const params = req.body
   console.log('params', params);
-  console.log('params', reportId);
-  res.send({
-    file: 'generated.pdf'
+  console.log('reportId', reportId);
+  res.render(`pages/${reportId}`, {params}, (err, html) => {
+    if (!err) {
+      console.log(err);
+    }
+    // console.log(html);
+    buildPdf(reportId, html, res);
   });
 });
 
-
-app.get('/pdf', function(req, res) {
-   
-    var externalURL = 'http://127.0.0.1:3000/index';
-      
-      requestify.get(externalURL).then(function (response) {
-        // Get the raw HTML response body
-        var html = response.body; 
-        
-        var config = {format: 'A4'}; // or format: 'letter' - see https://github.com/marcbachmann/node-html-pdf#options
-     
-       // Create the PDF
-        pdf.create(html, config).toFile('generated.pdf', function (err, result) {
-           if (err) return console.log(err);
-           console.log(result);
-           res.sendFile('generated.pdf',{ root: __dirname });
-        });
-     });
-});
-
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Report generator is running at 3000!');
 });
